54ce985e6f4787b3e627a2cc51b46a34d17ab199
Mutiu Omotosho <mutiuomotosho1001@gmail.com>
2023-04-11 10:32:51 -0700

    Add files via upload

---
+++ area_of_circle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+
+float pi = 3.142;
+
+int main() {
+
+    float radius;
+
+    /*print the and input the value of your radius of circle*/
+    printf("Welcome to calculate the value of area of circle\n");
+    /*Input the radius of the circle*/
+    printf("Kindly enter the radius of the circle here:");
+    scanf("%f", &radius);
+     float area_of_circle;
+     area_of_circle = pi * (radius * radius);
+     /*now print the result*/
+     printf("The value of the area of the circle is: %.2f", area_of_circle);
+    return 0;
+}
